04fd0af072f06b2e6b4612b56f93f01eb02fd17e
Ben Silverman <benjaminsilverman7@gmail.com>
2020-04-18 01:54:31 -0500

    changed some types

--- src/main/java/edu/brown/cs/term_project/TextSimilarity/IText.java
+++ src/main/java/edu/brown/cs/term_project/TextSimilarity/IText.java
@@ -9,5 +9,5 @@ public interface IText<W extends IWord> {
    * @param textType and integer telling which type of text (ie Entity  vs Vocab)
    * @return as hashmap that maps from word to frequency in text.
    */
-  Map<W, Double> getFreq(Integer textType);
+  Map<W, Integer> getFreq(Integer textType);
 }
--- src/main/java/edu/brown/cs/term_project/TextSimilarity/TextCorpus.java
+++ src/main/java/edu/brown/cs/term_project/TextSimilarity/TextCorpus.java
@@ -4,12 +4,14 @@ import java.util.HashMap;
 import java.util.Set;
 
 public class TextCorpus<W extends IWord, T extends IText<W>> {
-  private HashMap<W, Double> wordFreq;
+  private HashMap<W, Integer> wordFreq;
   private Set<T> nodes;
+  private int docNum;
 
-  public TextCorpus(HashMap<W, Double> wordFreq, Set<T> nodes) {
+  public TextCorpus(HashMap<W, Integer> wordFreq, Set<T> nodes, Integer docNum) {
     this.wordFreq = wordFreq;
     this.nodes = nodes;
+    this.docNum = docNum;
   }
 
   public Double getSimilarity(T src, T dst, Integer textType) {
