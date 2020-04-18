2b71ca308f8f7d4c0a969247995646939e5c5023
Ben Silverman <benjaminsilverman7@gmail.com>
2020-04-18 01:50:20 -0500

    IText now returns just a Map not HashMap

--- src/main/java/edu/brown/cs/term_project/TextSimilarity/IText.java
+++ src/main/java/edu/brown/cs/term_project/TextSimilarity/IText.java
@@ -1,6 +1,6 @@
 package edu.brown.cs.term_project.TextSimilarity;
 
-import java.util.HashMap;
+import java.util.Map;
 
 public interface IText<W extends IWord> {
 
@@ -9,5 +9,5 @@ public interface IText<W extends IWord> {
    * @param textType and integer telling which type of text (ie Entity  vs Vocab)
    * @return as hashmap that maps from word to frequency in text.
    */
-  HashMap<W, Double> getFreq(Integer textType);
+  Map<W, Double> getFreq(Integer textType);
 }
