7b67a0dd0442c50ea8a76ae497ebe0abed513cbe
Ben Silverman <benjaminsilverman7@gmail.com>
2020-04-18 03:26:19 -0500

    added in calculation for cosine similarity

--- src/main/java/edu/brown/cs/term_project/TextSimilarity/TextCorpus.java
+++ src/main/java/edu/brown/cs/term_project/TextSimilarity/TextCorpus.java
@@ -1,6 +1,7 @@
 package edu.brown.cs.term_project.TextSimilarity;
 
 import java.util.HashMap;
+import java.util.Map;
 import java.util.Set;
 
 public class TextCorpus<W extends IWord, T extends IText<W>> {
@@ -15,6 +16,30 @@ public class TextCorpus<W extends IWord, T extends IText<W>> {
   }
 
   public Double getSimilarity(T src, T dst, Integer textType) {
-    return null;
+    Map<W, Integer> srcMap = src.getFreq(textType);
+    Map<W, Integer> dstMap = dst.getFreq(textType);
+    Set<W> sharedWords = srcMap.keySet();
+    sharedWords.retainAll(dstMap.keySet());
+    double dotProduct = 0;
+    for (W w: sharedWords) {
+      dotProduct += this.getImportance(srcMap, w) * this.getImportance(dstMap, w);
+    }
+    return dotProduct / (Math.sqrt(this.getMagImportance(srcMap)
+        + Math.sqrt(this.getMagImportance(dstMap))));
   }
+
+  private Double getImportance(Map<W, Integer> wordMap, W word) {
+    Double normalizedTermFrequency =  wordMap.get(word) / (double) wordMap.size();
+    Double inverseDocumentFrequency = docNum / (double) wordFreq.get(word);
+    return normalizedTermFrequency * inverseDocumentFrequency;
+  }
+
+  private Double getMagImportance(Map<W, Integer> wordMap) {
+    double sum = 0;
+    for (W w: wordMap.keySet()) {
+      sum += this.getImportance(wordMap, w);
+    }
+    return sum;
+  }
+
 }
