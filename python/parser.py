efb3e6ecc455ce9cdde1649a0ff8ca650f724b0b
kshitijsachan <kshitijsachan39@gmail.com>
2020-04-20 09:49:24 -0500

    ported code to intellij

--- python/parser.py
+++ python/parser.py
@@ -1,12 +1,26 @@
+import argparse
 import newspaper
 
-def newspaper_package(url):
-	try:
-		article = newspaper.Article(url)
-		article.download()
-		article.parse()
-		print('finished parsing')
-		return article.text
-	except newspaper.article.ArticleException:
-		print("couldn't parse: " + url)
-		pass
+
+def get_text(url):
+    try:
+        article = newspaper.Article(url)
+        article.download()
+        article.parse()
+        print('finished parsing')
+        return article.text
+    except newspaper.article.ArticleException:
+        print("couldn't parse: " + url)
+        pass
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Get the text of an article from a URL.')
+    parser.add_argument('path', help='path to text file containing list of article URLs')
+    args = parser.parse_args()
+
+    with open(args.path) as f:
+        urls = f.read().splitlines()
+
+    for url_link in urls:
+        print(get_text(url_link))
