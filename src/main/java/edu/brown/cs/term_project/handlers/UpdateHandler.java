5f337d5c490a4f0b82fddfff27be8aaa1e3644dd
Ian Layzer <ianlayzer@gmail.com>
2020-04-21 14:43:58 -0400

    working on frontend

--- src/main/java/edu/brown/cs/term_project/handlers/UpdateHandler.java
+++ src/main/java/edu/brown/cs/term_project/handlers/UpdateHandler.java
@@ -1,5 +1,6 @@
 package edu.brown.cs.term_project.handlers;
 
+import com.google.gson.Gson;
 import spark.Request;
 import spark.Response;
 
@@ -22,18 +23,29 @@ public class UpdateHandler {
    * @return a JSON response with status and message.
    */
   public static String handle(Request request, Response response) {
-    // TODO: implement update handle()
-    String uri = "http://127.0.0.1:5000/data";
-    String toReturn = "";
-
+    String pythonEndpoint = "http://127.0.0.1:5000/scrape";
+    UpdateResponse updateResponse = new UpdateResponse(0, "");
     try {
-      toReturn = sendGet(uri);
+      String pythonResponse = sendGet(pythonEndpoint);
+
+      // WE NEED TO PARSE THE JSON STRING INTO A LIST OF SOMETHING THEN ADD TO DB
+
+
     } catch (Exception e) {
-      toReturn = "update: error connecting to python api.";
+      // there has been an error so update response to reflect that
+      updateResponse.setStatus(1);
+      updateResponse.setMessage(e.getMessage());
     }
-    return toReturn;
+    // convert to json and return
+    return new Gson().toJson(updateResponse);
   }
 
+  /**
+   * Sends an HTTP GET request to a url.
+   * @param uri the url to send the get request to
+   * @return a json string response
+   * @throws Exception
+   */
   private static String sendGet(String uri) throws Exception {
     HttpClient client = HttpClient.newHttpClient();
     HttpRequest request = HttpRequest.newBuilder()
@@ -41,5 +53,39 @@ public class UpdateHandler {
         .build();
     HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
     return response.body();
+  }
+
+  /**
+   * Class for response to a request to the Update API.
+   */
+  private static class UpdateResponse {
+    private int status; // 0 successful, 1 is error
+    private String message;
+
+    /**
+     * Constructor for the update response.
+     * @param status 0 successful, 1 error
+     * @param message error message if error
+     */
+    public UpdateResponse(int status, String message) {
+      this.status = status;
+      this.message = message;
+    }
+
+    /**
+     * Sets status.
+     * @param status new status
+     */
+    public void setStatus(int status) {
+      this.status = status;
+    }
+
+    /**
+     * Sets message.
+     * @param message new message
+     */
+    public void setMessage(String message) {
+      this.message = message;
+    }
   }
 }
