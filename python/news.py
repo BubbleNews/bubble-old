31677b5b7d1a16a044da08f6b4f54c8385734f76
kshitijsachan <kshitijsachan39@gmail.com>
2020-04-22 10:51:14 -0500

    connected java and python code but spark nlp issues

--- python/news.py
+++ python/news.py
@@ -7,7 +7,8 @@ import json
 # key for accessing google news api
 API_KEY = '77d7cb4756d44e13aea4a50d033d27e3'
 MINIMUM_ARTICLE_CHAR_LENGTH = 700
-BAD_CHARS = ['\n', '\r\n']
+BAD_CHARS = ['\r', '\n']
+
 
 def get_news():
     print("Connecting to Google News API...")
@@ -27,39 +28,54 @@ def get_news():
     # print(hour_ago)
 
     #
-    num_articles = 1
+    num_articles = 10
     # get top headlines
     top_headlines = newsapi.get_top_headlines(language='en',
-                                            page_size=num_articles)
+                                              page_size=num_articles)
+
+    articles_json = []
 
     articles = top_headlines['articles']
     # loop through articles and scrape article text with scraper
     for i, article in enumerate(articles):
-        print(i)
         url = article['url']
-        scraped_text = scrape_text(url)
+        scraped_title, scraped_authors, scraped_text = scrape_text(url)
         # threshold
         if len(scraped_text) < MINIMUM_ARTICLE_CHAR_LENGTH:
+            print('Article is too short: ' + url)
             # text is too short so delete article
-            del articles[i]
             with open('deleted_articles.txt', 'w') as f:
                 f.write(url)
         else:
-            # we want text
-            article['content'] = clean_text(scraped_text, BAD_CHARS)
+            articles_json.append(make_article_json(article, scraped_title, scraped_authors, clean_text(scraped_text, BAD_CHARS)))
+    return json.dumps(articles_json)
 
-    return json.dumps(articles)
+
+def make_article_json(article, scraped_title, scraped_authors, scraped_text):
+    return {
+        'author': scraped_authors,
+        'title': scraped_title,
+        'description': article['description'],
+        'url': article['url'],
+        'time_published': article['publishedAt'],
+        'content': scraped_text
+    }
+
 
 def scrape_text(url):
     try:
         article = Article(url)
         article.download()
         article.parse()
-        return article.text
+        return article.title, article.authors, article.text
     except ArticleException:
         print("couldn't parse: " + url)
+
 
 def clean_text(text, bad_chars):
     for char in bad_chars:
         text = text.replace(char, '')
     return text
+
+
+print(get_news())
