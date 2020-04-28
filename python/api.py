e7d259c5d953c0c0c3555ad0b753daf1a5d0a100
kshitijsachan <kshitijsachan39@gmail.com>
2020-04-28 14:33:51 -0500

    completed new python endpoint

--- python/api.py
+++ python/api.py
@@ -4,8 +4,13 @@ from news import get_news
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
+
 @app.route('/scrape', methods=['GET'])
 def home():
-    return get_news()
+    start_date = '2020-04-27'
+    end_date = '2020-04-28'
+    num_articles = 10
+    return get_news(start_date, end_date, num_articles)
+
 
 app.run()
