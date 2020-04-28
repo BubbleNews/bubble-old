5cba380ddfc6c669238f57862badb98bf7966a76
kshitijsachan <kshitijsachan39@gmail.com>
2020-04-28 14:36:42 -0500

    Ian's changes

--- python/api.py
+++ python/api.py
@@ -7,10 +7,12 @@ app.config["DEBUG"] = True
 
 @app.route('/scrape', methods=['GET'])
 def home():
-    start_date = '2020-04-27'
-    end_date = '2020-04-28'
-    num_articles = 10
-    return get_news(start_date, end_date, num_articles)
-
+    try:
+        start_date = str(request.args.get('startDate'))
+        end_date = str(request.args.get('endDate'))
+        num_articles = int(request.args.get('numArticles'))
+        get_news(start_date, end_date, num_articles)
+    except Exception as e:
+        return e
 
 app.run()
