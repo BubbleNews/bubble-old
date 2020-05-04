ab952ca1768d1f68ac3befac7375c93a74fd7d74
kshitijsachan <kshitijsachan39@gmail.com>
2020-05-03 20:07:00 -0500

    bar and chord now set up:

--- src/main/resources/spark/template/freemarker/diagram.ftl
+++ src/main/resources/spark/template/freemarker/diagram.ftl
@@ -8,6 +8,7 @@
 <body>
 <h>Diagram!</h>
 <div class="chord-chart"></div>
+<div class="bar-chart"></div>
 
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
--- src/main/resources/static/js/diagram.js
+++ src/main/resources/static/js/diagram.js
@@ -1,10 +1,11 @@
 function renderChord(parsed) {
-    const margin = {top: 40, right: 100, bottom: 40, left: 175};
-    const width = 500, height = Math.min(640, width);
-    const inner_width = width - margin.left - margin.right;
-    const inner_height = height - margin.top - margin.bottom;
+    const width = 500
+    const height = Math.min(640, width);
+    const textGap = 20;
     const outerRadius = Math.min(width, height) * 0.5 - 30;
     const innerRadius = outerRadius - 20;
+    const edgeOpacity = 0.67;
+    const padAngle = 0.15;
 
     // get data
     const arr = getChordDataMatrix(parsed);
@@ -16,7 +17,7 @@ function renderChord(parsed) {
         .attr('viewBox', [-width / 2, -height / 2, width, height]);
 
     const chords = d3.chord()
-        .padAngle(.15)
+        .padAngle(padAngle)
         .sortSubgroups(d3.descending)(matrix);
 
     const arc = d3.arc()
@@ -35,40 +36,28 @@ function renderChord(parsed) {
         .attr('fill', d => color(d.index))
         .attr('stroke', d => d3.rgb(color(d.index)).darker())
         .attr('d', arc);
-    //
-    const groupLabels = group.selectAll('g')
+
+    group.selectAll('g')
         .data(d => labels(d, titles))
         .join('g')
-        .attr('transform', d => transformText(d, outerRadius))
-        .attr('text-anchor', d => getTextAnchor(d));
+        .attr('transform', d => `rotate(${d.angle * 180 / Math.PI - 90}) 
+            translate(${outerRadius + textGap},0) rotate(${-1 * (d.angle * 180 / Math.PI - 90)})`)
+        .attr('text-anchor', d => getTextAnchor(d))
+        .append('text')
+            .text(d => d.title);
 
 
-    groupLabels.append('text')
-        .text(d => d.title);
-
-    //
-    // group.append('text')
-    //     .attr('text', (d,i) => titles[i])
-    //     .attr('x', 0)
-    //     .attr('y', 0);
     svg.append('g')
-        .attr('fill-opacity', .67)
+        .attr('fill-opacity', edgeOpacity)
     .selectAll('path')
         .data(chords)
         .join('path')
         .attr('d', ribbon)
-        .attr('fill', d => interpolateColor(d, color, matrix))
+        .attr('fill', d => interpolateColor(d, color))
         .attr('stroke', d => d3.rgb(color(d.target.index)).darker());
 }
 
-function transformText(d, outerRadius) {
-    const labelGap = 20;
-    const flipAngle = d.angle > Math.PI ? 'rotate(180)' : '';
-    return `rotate(${d.angle * 180 / Math.PI - 90}) translate(${outerRadius + labelGap},0) rotate(${-1 * (d.angle * 180 / Math.PI - 90)})`;
-}
-
 function getTextAnchor(d) {
-    console.log(d);
     if (d.angle > Math.PI) {
         return 'end'
     }
@@ -77,17 +66,13 @@ function getTextAnchor(d) {
 
 function labels(d, titles) {
     const midAngle = (d.startAngle + d.endAngle) / 2;
-    console.log({
-        angle: midAngle,
-        title: titles[d.index]
-    });
     return [{
         angle: midAngle,
         title: titles[d.index]
     }];
 }
 
-function interpolateColor(d, color, matrix) {
+function interpolateColor(d, color) {
     const srcIndex = d.source.index;
     const targetIndex = d.target.index;
     const srcColor = color(srcIndex);
@@ -99,19 +84,17 @@ function getChordDataMatrix(parsed) {
     const edges = parsed['edges'];
     const numVertices = parsed['numVertices'];
     const titles = [];
+    // map from article ID -> index in matrix array
+    const indices = {};
 
     // this will be data matrix for the chord diagram
     const matrix = [];
     // initialize matrix with empty values
     for(let i = 0; i < numVertices; i++) {
         matrix[i] = new Array(numVertices);
-
         // set weight from article to itself to 0 because articles can't be similar to themselves
         matrix[i][i] = 0;
     }
-
-    // map from article ID -> index in matrix array
-    const indices = {};
 
     // fill in matrix with distances
     edges.forEach(edge => {
@@ -142,6 +125,158 @@ function getIndex(id, indices) {
     return indices[id];
 }
 
+
+
+
+
+
+////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////
+
+/**
+ *
+ * @param data Object of the form:
+ * {
+ *     totalEntities: {},
+ *     totalTitle: {},
+ *     totalWords: {}
+ * }
+ * @param type
+ */
+function renderBarPlot(data, type) {
+    console.log(data);
+    const width = 1000;
+    const height = 800;
+    const margin = {left: 40, right: 10, top: 10, bottom: 0};
+    const innerWidth = width - margin.left - margin.right;
+    const innerHeight = height - margin.top - margin.bottom;
+    const numBarsToDisplayThresholdPercent = 0.9;
+    const labelPadding = 10;
+    const fillColor = {
+        entity: 'steelblue',
+        title: 'red',
+        text: 'orange'
+    };
+
+    const words = formatBarPlotData(data, type);
+    const relevantWords = sliceWords(words, numBarsToDisplayThresholdPercent);
+    console.log(relevantWords);
+
+
+    const svg = d3.select(".bar-chart")
+        .append("svg")
+        .attr('width', width)
+        .attr('height', height)
+        .append('g')
+        .attr('transform', `translate(${margin.left}, ${margin.top})`);
+
+    const x = d3.scaleLinear()
+        .domain(d3.extent(relevantWords.map(d => d.value)))
+        .range([0, innerWidth])
+        .nice();
+
+    const y = d3.scaleBand()
+        .domain(d3.range(relevantWords.length))
+        .range([0, innerHeight])
+        .padding(0.1);
+
+    svg.append('g')
+        .attr('class', 'bar-labels')
+        .attr('transform', `translate(${margin.left},0)`)
+        .selectAll('text')
+        .data(relevantWords)
+        .join('text')
+        .attr('y', (d, i) => y(i))
+        .attr('text-anchor', 'end')
+        .attr('dy', y.bandwidth()/2)
+        .attr('dx', -labelPadding)
+        .text(d => d.word);
+
+
+    const rect = svg.append('g')
+        .attr('transform', `translate(${margin.left},0)`)
+        .selectAll('rect')
+        .data(relevantWords)
+        .join('rect')
+            .attr('x', 0)
+            .attr('y', (d, i) => y(i))
+            .attr('width', d => x(d.value))
+            .attr('height', y.bandwidth())
+            .attr('fill', d => fillColor[d.type]);
+
+    rect.append()
+}
+
+function sliceWords(words, thresholdPercent) {
+    let sum = 0;
+    words.forEach(w => sum += w.value);
+    const threshold = sum * thresholdPercent;
+
+    let numItems = 0;
+    let increasingSum = 0;
+    while (increasingSum < threshold) {
+        increasingSum += words[numItems].value;
+        numItems++;
+    }
+
+    return words.slice(0, numItems);
+}
+
+/**
+ *
+ * @param hashmaps
+ * @param type
+ * @returns []
+ */
+function formatBarPlotData(hashmaps, type) {
+    const entities = hashmaps.totalEntities;
+    const text = hashmaps.totalWords;
+    const title = hashmaps.totalTitle;
+    const data = [];
+
+    switch (type) {
+        case 'entity':
+            addToArray(entities, type, data);
+            break;
+
+        case 'text':
+            addToArray(text, type, data);
+            break;
+
+        case 'title':
+            addToArray(title, type, data);
+            break;
+
+        default:
+            addToArray(entities, 'entity', data);
+            addToArray(text, 'text', data);
+            addToArray(title, 'title', data);
+    }
+
+    data.sort((a,b) => b.value - a.value);
+    return data;
+}
+
+function addToArray(map, type, arr) {
+    // console.log('called', arr.length);
+    for (let word in map) {
+        arr.push({
+            type: type,
+            word: word,
+            value: map[word]
+        })
+    }
+}
+
+
+
+
+
+
+
 function getClusterDetails(clusterId) {
     let clusterUrl = 'api/details';
     // add id to cluster base url
@@ -149,7 +284,8 @@ function getClusterDetails(clusterId) {
     // send get request
     $.get(clusterUrl, response => {
         const parsed = JSON.parse(response);
-        renderChord(parsed)
+        renderChord(parsed);
+        renderBarPlot(parsed, 'all');
     });
 }
 
@@ -160,8 +296,9 @@ function getEdgeDetails(id1, id2) {
     // send get request
     $.get(clusterUrl, response => {
         const parsed = JSON.parse(response);
+        // renderBarPlot(parsed);
     });
 }
 
-getClusterDetails(9);
-getEdgeDetails(1, 343);
+getClusterDetails(6);
+getEdgeDetails(23, 40);
